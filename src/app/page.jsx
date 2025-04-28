7a5c58ca3d878dcfb8d55f6c59ffad166651bec3
“joanomana” <joansebastianomasu@gmail.com>
2025-04-28 17:41:06 +0200

    feat: :tada: Create init files

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home (){
+  return(
+    <div className=" bg-gray-200 h-screen p-2 flex flex-col items-center">
+      <h1>Hola</h1>
+      <div className="flex flex-col items-center justify-center p-2 bg-white rounded-lg shadow-md max-w-2/4">
+        <h1>Crea tu receta</h1>
+
+
+      </div>
+
+    </div>
+  )
+}
